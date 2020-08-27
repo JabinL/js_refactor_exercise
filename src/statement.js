88ddbc1cca7618535ae0e7c8ee1d03b958290cb8
Jabin <2099008974@qq.com>
2020-08-28 00:21:51 +0800

    [exercise][Jabin] refactor: extract format function

--- src/statement.js
+++ src/statement.js
@@ -1,12 +1,16 @@
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+function format(amount) {
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+  return format(amount);
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
